abb89050b24132b3a966d3be4390af20910195cc
Andreani Jean <andreanijean@macbook-pro-de-andreani.home>
2022-10-04 13:24:55 +0200

    add script start

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', routes)
-app.get('/', function (req, res) {
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/index.html'))
-})
+)
 app.listen(process.env.PORT || 1337, () => {
     console.log('Your app is running')
 })
